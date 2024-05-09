d119ae0c116c2e044f8e94449f312f2b98c7e392
alechkos <93551621+alechkos@users.noreply.github.com>
2024-05-09 22:44:29 +0300

    fix: revert unneccessary changes
    Co-authored-by: Pavel <87754563+NiPavel@users.noreply.github.com>

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -4,14 +4,14 @@ import { Sequelize } from 'sequelize'
 dotenv.config()
 
 // It is a production db connection, when we will finish this proj -> uncomment this
-// const DB_NAME = process..env.NODE_ENV === 'dev' ?process..env.TEST_DB_NAME!: process..env.DB_NAME!
+// const DB_NAME = process.env.NODE_ENV === 'dev' ?process.env.TEST_DB_NAME!: process.env.DB_NAME!
 // export const sequelize = new Sequelize(
 //   DB_NAME,
-//   process..env.DB_USER!,
-//   process..env.DB_PASSWORD,
+//   process.env.DB_USER!,
+//   process.env.DB_PASSWORD,
 //   {
 //     dialect: 'postgres',
-//     host: process..env.DB_HOST,
+//     host: process.env.DB_HOST,
 //     port: 5432
 //   }
 // )
