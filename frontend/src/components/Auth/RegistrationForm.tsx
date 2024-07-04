9dcb893dbdd90abdd4c2eff42c9cfb9bb5b9ee30
alechkos <93551621+alechkos@users.noreply.github.com>
2024-07-04 05:36:33 +0300

    fix: decrease the maximum user age to 90

--- frontend/src/components/Auth/RegistrationForm.tsx
+++ frontend/src/components/Auth/RegistrationForm.tsx
@@ -146,7 +146,7 @@ function RegistrationForm() {
             name="birthYear"
             type="number"
             max={new Date().getFullYear() - 16}
-            min={new Date().getFullYear() - 120}
+            min={new Date().getFullYear() - 90}
             required
             className={`relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-${mainColor} focus:outline-none focus:ring-${mainColor} sm:text-sm`}
             placeholder="Birth year"
