1ddc8f3e54a4fbd86dc7e1048fba11d9de101215
alechkos <93551621+alechkos@users.noreply.github.com>
2024-08-25 20:44:12 +0300

    feat: add assest for sorting algorithms

--- frontend/src/components/Home/HomePageData.ts
+++ frontend/src/components/Home/HomePageData.ts
@@ -18,6 +18,18 @@ import hashTablePhoto from "../../assets/Gallery/HashTablePhoto.png";
 import hashTableGif from "../../assets/Gallery/HashTableGif.gif";
 import BFImage from "../../assets/Gallery/BFImage.png";
 import BFGif from "../../assets/Gallery/BFGif.gif";
+import insertionSortGif from "../../assets/Gallery/insertionSortGif.gif";
+import insertionSortImg from "../../assets/Gallery/insertionSortImage.png";
+import countingSortGif from "../../assets/Gallery/countingSortGif.gif";
+import countingSortImg from "../../assets/Gallery/countingSortImg.png";
+import bucketSortGif from "../../assets/Gallery/bucketSortGif.gif";
+import bucketSortImg from "../../assets/Gallery/bucketSortImg.png";
+import mergeSortGif from "../../assets/Gallery/mergeSortGif.gif";
+import mergeSortImg from "../../assets/Gallery/mergeSortImg.png";
+import quickSortGif from "../../assets/Gallery/quickSortGif.gif";
+import quickSortImg from "../../assets/Gallery/quickSortImg.png";
+import radixSortGif from "../../assets/Gallery/radixSortGif.gif";
+import radixSortImg from "../../assets/Gallery/radixSortImg.png";
 import { RoutePaths } from "../../Routes/RoutePaths";
 
 const HomePageData = [
@@ -90,6 +102,48 @@ const HomePageData = [
     image: BFImage,
     url: RoutePaths.PRIM,
     type: "Graph",
+  },
+  {
+    title: "Insertion Sort",
+    gif: insertionSortGif,
+    image: insertionSortImg,
+    url: RoutePaths.INSERTION_SORT,
+    type: "Sort",
+  },
+  {
+    title: "Counting Sort",
+    gif: countingSortGif,
+    image: countingSortImg,
+    url: RoutePaths.COUNTING_SORT,
+    type: "Sort",
+  },
+  {
+    title: "Bucket Sort",
+    gif: bucketSortGif,
+    image: bucketSortImg,
+    url: RoutePaths.BUCKET_SORT,
+    type: "Sort",
+  },
+  {
+    title: "Merge Sort",
+    gif: mergeSortGif,
+    image: mergeSortImg,
+    url: RoutePaths.MERGE_SORT,
+    type: "Sort",
+  },
+  {
+    title: "Quick Sort",
+    gif: quickSortGif,
+    image: quickSortImg,
+    url: RoutePaths.QUICK_SORT,
+    type: "Sort",
+  },
+  {
+    title: "Radix Sort",
+    gif: radixSortGif,
+    image: radixSortImg,
+    url: RoutePaths.RADIX_SORT,
+    type: "Sort",
   },
 ];
 
